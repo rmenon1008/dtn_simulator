b5aafded8f862848787bccd407a7cd3614155713
Dominick Ta <domta@cs.washington.edu>
2023-04-23 23:45:22 -0700

    refactor Dtn/fix bug, fix storage, all tests work

--- peripherals/routing_protocol/test/dtn/dtn_test.py
+++ peripherals/routing_protocol/test/dtn/dtn_test.py
@@ -15,7 +15,8 @@ Method used to setup DTN objects for this file's unit tests.
 def setup():
     # setup a dummy model object used by the DTN objects.
     schedule = mesa.time.RandomActivation(mesa.Model())
-    dummy_model = mock({"schedule": schedule})
+    agents_dict = dict()
+    dummy_model = mock({"schedule": schedule, "agents": agents_dict})
 
     # setup the 5 nodes (0, 1, 2, 3, 4) + save them in the dtn_dict for easy access.
     # we set them up wrapped in a "spy" so we can easily peek into method call stats.
@@ -26,15 +27,46 @@ def setup():
     dtn_dict[3] = spy(Dtn(3, dummy_model))
     dtn_dict[4] = spy(Dtn(4, dummy_model))
 
-    # wire-up the dummy_model to use the dtn_dict for lookups.
-    when(dummy_model).get_routing_protocol_object(0).thenReturn(dtn_dict[0])
-    when(dummy_model).get_routing_protocol_object(1).thenReturn(dtn_dict[1])
-    when(dummy_model).get_routing_protocol_object(2).thenReturn(dtn_dict[2])
-    when(dummy_model).get_routing_protocol_object(3).thenReturn(dtn_dict[3])
-    when(dummy_model).get_routing_protocol_object(4).thenReturn(dtn_dict[4])
+    dummy_agent0 = mock({"routing_protocol": dtn_dict[0]})
+    dummy_agent1 = mock({"routing_protocol": dtn_dict[1]})
+    dummy_agent2 = mock({"routing_protocol": dtn_dict[2]})
+    dummy_agent3 = mock({"routing_protocol": dtn_dict[3]})
+    dummy_agent4 = mock({"routing_protocol": dtn_dict[4]})
+    agents_dict[0] = dummy_agent0
+    agents_dict[1] = dummy_agent1
+    agents_dict[2] = dummy_agent2
+    agents_dict[3] = dummy_agent3
+    agents_dict[4] = dummy_agent4
+
+    neighbors_for_0 = [{"id": 1, "connected": True}, 
+                       {"id": 2, "connected": True}, 
+                       {"id": 3, "connected": True}, 
+                       {"id": 4, "connected": True}]
+    neighbors_for_1 = [{"id": 0, "connected": True}, 
+                       {"id": 2, "connected": True}, 
+                       {"id": 3, "connected": True}, 
+                       {"id": 4, "connected": True}]
+    neighbors_for_2 = [{"id": 0, "connected": True}, 
+                       {"id": 1, "connected": True}, 
+                       {"id": 3, "connected": True}, 
+                       {"id": 4, "connected": True}]
+    neighbors_for_3 = [{"id": 0, "connected": True}, 
+                       {"id": 1, "connected": True}, 
+                       {"id": 2, "connected": True}, 
+                       {"id": 4, "connected": True}]
+    neighbors_for_4 = [{"id": 0, "connected": True}, 
+                       {"id": 1, "connected": True}, 
+                       {"id": 2, "connected": True}, 
+                       {"id": 3, "connected": True}]
+
+    when(dummy_model).get_neighbors(dummy_agent0).thenReturn(neighbors_for_0)
+    when(dummy_model).get_neighbors(dummy_agent1).thenReturn(neighbors_for_1)
+    when(dummy_model).get_neighbors(dummy_agent2).thenReturn(neighbors_for_2)
+    when(dummy_model).get_neighbors(dummy_agent3).thenReturn(neighbors_for_3)
+    when(dummy_model).get_neighbors(dummy_agent4).thenReturn(neighbors_for_4)
 
     # link-up the nodes as follows with the respective timestamps...
-    # 0 -> 1:  @3-5
+    # 0 -> 1:  @3-inf
     # 0 -> 2:  @0->inf
     # 2 -> 1:  @0->inf
     # 0 -> 3:  @0->inf
@@ -60,6 +92,8 @@ def test_handle_bundle_best_route_indirect(setup):
 
     # have node 0 handle the Bundle.
     dtn_dict[0].handle_bundle(bundle)
+    dtn_dict[0].refresh() # forward to 2
+    dtn_dict[2].refresh() # forward to 1
 
     # assert that the bundle was sent to node 2 and node 1.
     verify(dtn_dict[0], times=1).handle_bundle(...)
@@ -75,6 +109,7 @@ Tests sending a Bundle when the best route switches from an indirect one to a di
 def test_handle_bundle_best_route_direct(setup):
     schedule, dtn_dict = setup
 
+
     # create the Bundle to send.
     bundle = Bundle(0, 1, Payload(), schedule.time)
 
@@ -85,6 +120,9 @@ def test_handle_bundle_best_route_direct(setup):
 
     # have node 0 handle the Bundle.
     dtn_dict[0].handle_bundle(bundle)
+    dtn_dict[0].refresh() # forward to 1, calls 1.handle_bundle()
+    dtn_dict[1].refresh() # does nothing, assert times=1
+    dtn_dict[2].refresh() # does nothing, assert times=0
 
     # assert that the bundle was only sent to node 1.
     verify(dtn_dict[0], times=1).handle_bundle(...)
@@ -94,7 +132,9 @@ def test_handle_bundle_best_route_direct(setup):
 """
 Tests sending a Bundle when the best route is indirect.
 
-(@timestamp = 0, the best route is 0->2->1)
+(@timestamp = 0, in default contact plan there is no route to 4)
+after adding contact...
+(@timestamp = 0, best route is 0->3->4)
 """
 def test_handle_bundle_stores_bundle_sends_once_linked(setup):
     schedule, dtn_dict = setup
@@ -104,6 +144,7 @@ def test_handle_bundle_stores_bundle_sends_once_linked(setup):
 
     # have node 0 handle the Bundle.
     dtn_dict[0].handle_bundle(bundle)
+    dtn_dict[0].refresh() # attempt to forward, but there's not a route
 
     # assert that the bundle was not sent to node 4.
     verify(dtn_dict[0], times=1).handle_bundle(...)
@@ -117,6 +158,7 @@ def test_handle_bundle_stores_bundle_sends_once_linked(setup):
         end_time=sys.maxsize,
         rate=100
     )
+    dtn_dict[0].refresh() # successfully forward to 3
     dtn_dict[3].add_contact(
         source=3,
         dest=4,
@@ -124,8 +166,10 @@ def test_handle_bundle_stores_bundle_sends_once_linked(setup):
         end_time=sys.maxsize,
         rate=100
     )
+    dtn_dict[3].refresh() # successfully forward to 4
 
     # assert that the new contact caused 4 to receive the bundle.
+    verify(dtn_dict[0], times=1).handle_bundle(...)
     verify(dtn_dict[3], times=1).handle_bundle(...)
     verify(dtn_dict[4], times=1).handle_bundle(...)
 
@@ -171,7 +215,7 @@ def test_construct_dtn_from_json():
 
     # have node 10 handle the Bundle.
     dtn_dict[10].handle_bundle(bundle)
-    dtn_dict[10].refresh()
+    dtn_dict[10].refresh() # forward to 1
 
     # assert that the bundle was sent directly from node 10 to node 1.
     verify(dtn_dict[10], times=1).handle_bundle(...)
